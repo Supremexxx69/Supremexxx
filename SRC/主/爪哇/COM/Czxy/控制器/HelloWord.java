d87c17e7d421909dfc83d5a3dd26b546127f9477
Supremexxx69 <53336616+Supremexxx69@users.noreply.github.com>
2019-07-28 15:58:03 +0800

    Update and rename src/main/java/com/czxy/controller/HelloWord.java to SRC/主/爪哇/COM/Czxy/控制器/HelloWord.java

--- SRC/主/爪哇/COM/Czxy/控制器/HelloWord.java
+++ SRC/主/爪哇/COM/Czxy/控制器/HelloWord.java
@@ -5,5 +5,7 @@ public class HelloWord {
     public static void main(String[] args) {
 
         System.out.println("sss");
+        System.out.println("sss");
+        System.out.println("sss");
     }
 }
